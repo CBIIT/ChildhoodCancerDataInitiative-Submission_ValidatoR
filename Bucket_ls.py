39bd1030fd4d6a68a5cf178671efa56aae8cf5a9
svburke <42675174+svburke@users.noreply.github.com>
2023-02-10 13:31:51 -0600

    Update Bucket_ls.py

--- Bucket_ls.py
+++ Bucket_ls.py
@@ -1,16 +1,15 @@
-import subprocess
 import argparse
 import argcomplete
 from datetime import date
 import os
-
+import boto3
 
 parser = argparse.ArgumentParser(
                     prog='Bucket_ls.py',
                     description='A script to create a ls --recursive list of bucket for ingest into the validation scripts. This is used when the pipeline needs to be run on a machine (VM) that does not have aws access, and this output is then transfered to that machine.',
                     )
 
-parser.add_argument( '-s', '--s3_bucket', help="The s3 bucket location, in the format of 's3://bucket/name/here/'")
+parser.add_argument( '-s', '--s3_bucket', help="The s3 bucket location, in the format of 'bucket' when compared to the url of 's3://bucket/name/here/'")
 parser.add_argument( '-o', '--output', help="The directory location of the output file. The default is the current directory.",default="./")
 
 argcomplete.autocomplete(parser)
@@ -30,11 +29,11 @@ output=os.path.abspath(output)
 
 
 #Test bucket to make sure expected parts are there.
-if s3_bucket.find("s3://") != 0:
-    s3_bucket="s3://"+s3_bucket
+if s3_bucket.find("s3://") == 0:
+    s3_bucket=s3_bucket.replace("s3://","")
 
-if s3_bucket[len(s3_bucket)-1]!="/":
-    s3_bucket=s3_bucket+"/"
+if s3_bucket[len(s3_bucket)-1]=="/":
+    s3_bucket=s3_bucket[:len(s3_bucket)-1]
 
 #Make sure dir location is setup correctly
 if output[len(output)-1]!="/":
@@ -42,29 +41,18 @@ if output[len(output)-1]!="/":
 
 date=refresh_date()
 
-#Pull file from AWS CLI
-subprocess.run([f"aws s3 ls --recursive {s3_bucket} > bucket.txt"], shell=True)
+#Get s3 session setup
+session=boto3.Session()
 
-#Read in file
-bucket_file=open("bucket.txt", "r")
+s3=session.resource('s3')
 
-bucket_files=bucket_file.read()
+my_bucket=s3.Bucket(s3_bucket)
 
-#Clean up file
-while bucket_files.find('  ') != -1:
-    bucket_files=bucket_files.replace('  ', ' ')
+#List the bucket's file size and file and write out.
+tsv_text= open(f"{output}{s3_bucket}_{date}.tsv","w")
 
-bucket_files=bucket_files.replace(' ','\t')
+for my_bucket_obj in my_bucket.objects.all():
+    write_down=str(my_bucket_obj.size)+"\t"+my_bucket_obj.key+"\n"
+    n=tsv_text.write(write_down)
 
-#Clean up file name
-bucket_name=s3_bucket.split("s3://")[1]
-bucket_name=bucket_name.replace("/","_")
-bucket_name=bucket_name[:-1]
-
-#Remove s3 output file
-subprocess.run(["rm bucket.txt"], shell=True)
-
-#Write out TSV for input into script.
-tsv_text= open(f"{output}{bucket_name}_{date}.tsv","w")
-n=tsv_text.write(bucket_files)
 tsv_text.close()
