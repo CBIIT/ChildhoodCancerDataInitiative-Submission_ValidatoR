81b7d278bfcbe852608b726b887b67158017b093
svburke <42675174+svburke@users.noreply.github.com>
2023-05-15 10:36:09 -0500

    Update CCDI-Submission_ValidatoR.R

--- CCDI-Submission_ValidatoR.R
+++ CCDI-Submission_ValidatoR.R
@@ -61,7 +61,7 @@ option_list = list(
 )
 
 #create list of options and values for file input
-opt_parser = OptionParser(option_list=option_list, description = "\nCCDI-Submission_ValidationR v1.1.0")
+opt_parser = OptionParser(option_list=option_list, description = "\nCCDI-Submission_ValidationR v1.1.1")
 opt = parse_args(opt_parser)
 
 #If no options are presented, return --help, stop and print the following message.
